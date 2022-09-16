9d41ab29a2c6d62439307210b94d92388033a660
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-09-16 23:51:59 +0600

    adding cart context and shoping icon as well as cart dropdown

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { CartProvider } from './contexts/cart.context';
+import { ProductsProvider } from './contexts/products.context';
 import { UserProvider } from './contexts/user.context';
 
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
@@ -14,7 +17,11 @@ root.render(
     <React.StrictMode>
         <BrowserRouter>
             <UserProvider>
-                <App />
+                <ProductsProvider>
+                    <CartProvider>
+                        <App />
+                    </CartProvider>
+                </ProductsProvider>
             </UserProvider>
         </BrowserRouter>
     </React.StrictMode>
