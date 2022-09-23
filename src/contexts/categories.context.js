a8eef8331c1b5ce8037f4f50e04e06175c934b3e
Fahimul Hoque Jason <fahimulhoquejason@gmail.com>
2022-09-23 23:28:56 +0600

    merged with firebase-database

--- src/contexts/categories.context.js
+++ src/contexts/categories.context.js
@@ -2,17 +2,13 @@ import { createContext, useEffect, useState } from 'react';
 
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
-// import SHOP_DATA from '../shop-data';
-
 export const CategoriesContext = createContext({
     categoriesMap: {},
 });
 
 export const CategoriesProvider = ({ children }) => {
     const [categoriesMap, setCategories] = useState({});
-    // useEffect(() => {
-    //     addCollectionsAndDocuments('categories', SHOP_DATA);
-    // }, []);
+
     useEffect(() => {
         const getCategoriesMap = async () => {
             const categoryMap = await getCategoriesAndDocuments();
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,38 +1,12 @@
-// import { Fragment, useContext } from "react";
 import { Routes, Route } from 'react-router-dom';
-// import { useContext } from 'react';
 
-// import ProductCard from "../../components/product-card/product-cart.component";
-// import CategoryPreview from '../../components/category-preview/category-preview.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-
-// import { CategoriesContext } from "../../contexts/categories.context";
 
 import './shop.style.scss';
 
 const Shop = () => {
-    // const { categoriesMap } = useContext(CategoriesContext);
-
     return (
-        // <Fragment>
-        //     {Object.keys(categoriesMap).map((title) => (
-        //         <Fragment key={title}>
-        //             <h2>{title}</h2>
-        //             <div className='products-container'>
-        //                 {categoriesMap[title].map((product) => (
-        //                     <ProductCard key={product.id} product={product} />
-        //                 ))}
-        //             </div>
-        //         </Fragment>
-        //     ))}
-        // </Fragment>
-        // <div className='shop-container'>
-        //     {Object.keys(categoriesMap).map((key) => {
-        //         const products = categoriesMap[key];
-        //         return <CategoryPreview key={key} title={key} products={products} />;
-        //     })}
-        // </div>
         <Routes>
             <Route index element={<CategoriesPreview />} />
             <Route path=':category' element={<Category />} />
